d11be53229134347363bc44c81ce821a511dd0c6
Barta Gábor <emoryy@gmail.com>
2017-05-26 17:44:03 +0200

    add clickOutsideToCloseException

--- addon/components/tether-dialog.js
+++ addon/components/tether-dialog.js
@@ -3,11 +3,41 @@ import ModalDialog from './modal-dialog';
 import layout from '../templates/components/tether-dialog';
 
 const { dasherize } = Ember.String;
-const { computed, get } = Ember;
+const { $, computed, get, guidFor } = Ember;
+
 const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
 export default ModalDialog.extend({
   layout,
+
+  parentIdClass: Ember.computed(function() {
+    return guidFor(this);
+  }),
+
+  didInsertElement() {
+    if (!this.get('clickOutsideToClose')) {
+      return;
+    }
+
+    const handleClick = (event) => {
+      const exception = this.get('clickOutSideToCloseException');
+      const eventTarget = $(event.target);
+      const clickedOnDialog = eventTarget.closest(`.${guidFor(this)}`).length;
+      const clickedOnException = exception && eventTarget.closest(exception).length;
+      if (!clickedOnDialog && !clickedOnException) {
+        this.send('close');
+      }
+    };
+    const registerClick = () => $(document).on(`click.ember-modal-dialog-${guidFor(this)}`, handleClick);
+
+    // setTimeout needed or else the click handler will catch the click that spawned this modal dialog
+    setTimeout(registerClick);
+
+    if (isIOS) {
+      const registerTouch = () => $(document).on(`touchend.ember-modal-dialog-${guidFor(this)}`, handleClick);
+      setTimeout(registerTouch);
+    }
+  },
 
   targetAttachmentClass: computed('targetAttachment', function() {
     let targetAttachment = this.get('targetAttachment') || '';
