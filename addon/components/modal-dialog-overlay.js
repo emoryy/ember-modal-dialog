a688e0c3c34a4edf25cc05ff093a9cfcda03a9fd
Barta Gábor <emoryy@gmail.com>
2018-06-05 15:07:36 +0200

    overlay with tether

--- addon/components/modal-dialog-overlay.js
+++ addon/components/modal-dialog-overlay.js
@@ -11,24 +11,4 @@ export default Ember.Component.extend({
     }
   },
 
-  didInsertElement: function(...args) {
-    this._super(...args);
-    if (!this.get('forTether')) {
-      return;
-    }
-
-    const observer = new MutationObserver((mutationsList) => {
-      const parentNode = this.element.parentNode;
-      for (let mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-          const transforms = parentNode.style.transform.match(/(\d*px)/g);
-          this.element.style.transform =  `translateX(-${transforms[0]}) translateY(-${transforms[1]})`;
-        }
-      }
-    });
-
-    const config = { attributes: true };
-    observer.observe(this.element.parentNode, config);
-  },
-
 });
